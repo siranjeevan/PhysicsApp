18ed5a6bcb7f656506691788209a82bb7333cbfa
siranjeevan <smartjeevith2k@gmail.com>
2025-05-11 17:24:27 +0530

    Initial Commit

---
+++ Phy/PhyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhyApp.swift
+//  Phy
+//
+//  Created by IOSTEAM   on 5/11/25.
+//
+
+import SwiftUI
+
+@main
+struct PhyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PhyTests/PhyTests.swift
@@ -0,0 +1,16 @@
+//
+//  PhyTests.swift
+//  PhyTests
+//
+//  Created by IOSTEAM   on 5/11/25.
+//
+
+import Testing
+
+struct PhyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
